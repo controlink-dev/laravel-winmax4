c85843264b209a793ee46dcfff99459cc9accc82
inesc <ines.pereira@controlink.pt>
2024-07-02 12:50:47 +0100

    Remove debug function from Winmax4Controller
    The debug function (dd) used for checking the POST request parameters in the Winmax4Controller has been removed. This function was previously displaying sensitive information like Company code, username, password, and terminal code. Now, the POST request uses the GuzzleHttp client to send data in JSON format without displaying it.

--- src/app/Http/Controllers/Winmax4Controller.php
+++ src/app/Http/Controllers/Winmax4Controller.php
@@ -29,16 +29,6 @@ class Winmax4Controller extends Controller
         $password = $request->password;
         $n_terminal = $request->n_terminal;
 
-        dd([
-            'verify' => config('winmax4.verify_ssl_guzzle'),
-            'json' => [
-                'Company' => $company_code,
-                'UserLogin' => $username,
-                'Password' => $password,
-                'TerminalCode' => $n_terminal,
-            ],
-        ]);
-
         $client = new \GuzzleHttp\Client();
         $response = $client->post($url . '/Account/GenerateToken', [
             'verify' => config('winmax4.verify_ssl_guzzle'),
