0c1b54ffec28d4785081b76c171efc9a336d5cce
inesc <ines.pereira@controlink.pt>
2024-07-02 13:16:16 +0100

    Remove HasUuids trait from Winmax4Settings model
    The HasUuids trait has been removed from the Winmax4Settings model. The decision was made as it was no longer being used in the context of the project. This change simplifies the model and removes unnecessary lines of code.

--- src/app/Models/Winmax4Settings.php
+++ src/app/Models/Winmax4Settings.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Winmax4Settings extends Model
 {
-    use HasFactory, SoftDeletes, HasUuids;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'url',
