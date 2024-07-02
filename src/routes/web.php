1204c5dd66089a22782aa71de3cd1076457d59a7
inesc <ines.pereira@controlink.pt>
2024-07-02 18:28:56 +0100

    Remove unnecessary line in web routes
    An unnecessary blank line in the web routes file within the Winmax4 middleware group has been removed. This deletion aims to clean up the code and improve readability.

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,6 @@ use Controlink\LaravelWinmax4\app\Http\Controllers\Winmax4Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])->prefix('winmax4')->group(function () {
-
     Route::get('/getWinmax4Settings', [Winmax4Controller::class, 'getWinmax4Settings'])->name('winmax4.getWinmax4Settings');
     Route::post('/generateToken', [Winmax4Controller::class, 'generateToken'])->name('winmax4.generateToken');
 });
