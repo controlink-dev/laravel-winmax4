bb0b648658610b096ac9808989d8d85725c7ee7e
inesc <ines.pereira@controlink.pt>
2024-07-08 11:51:14 +0100

    Improve readability of web.php routing file
    Reformatted the 'web.php' routing file to enhance code readability. The modifications involve mainly adding spacing between the 'Route::get' statements to separate them visually and make the structure more comprehensible.

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,6 +8,7 @@ Route::middleware(['web'])->prefix('winmax4')->group(function () {
     Route::post('/generateToken', [Winmax4Controller::class, 'generateToken'])->name('winmax4.generateToken');
 
     Route::get('/getCurrencies', [Winmax4Controller::class, 'getCurrencies'])->name('winmax4.getCurrencies');
+
     Route::get('/getDocumentTypes', [Winmax4Controller::class, 'getDocumentTypes'])->name('winmax4.getDocumentTypes');
 
     Route::get('/getFamilies', [Winmax4Controller::class, 'getFamilies'])->name('winmax4.getFamilies');
@@ -15,6 +16,7 @@ Route::middleware(['web'])->prefix('winmax4')->group(function () {
     Route::get('/getSubSubFamilies/{sub_family_id}', [Winmax4Controller::class, 'getSubSubFamilies'])->name('winmax4.getSubSubFamilies');
 
     Route::get('/getTaxes', [Winmax4Controller::class, 'getTaxes'])->name('winmax4.getTaxes');
+
     Route::get('/getArticles', [Winmax4Controller::class, 'getArticles'])->name('winmax4.getArticles');
 
     Route::get('/getEntities', [Winmax4Controller::class, 'getEntities'])->name('winmax4.getEntities');
