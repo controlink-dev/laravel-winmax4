880847641eb2216a94744630e204e05d640a7eea
InesPereiira <inescunhachacatas@gmail.com>
2024-07-15 21:53:47 +0100

    Implement SoftDeletes in Winmax4Entity model
    In the Winmax4Entity model, the SoftDeletes trait is now implemented. The code ensures that this trait is added if it doesn't already exist, providing soft delete functionality where needed. Several lines of code were modified to accommodate this change and improve application robustness.

--- src/app/Models/Winmax4Entity.php
+++ src/app/Models/Winmax4Entity.php
@@ -3,9 +3,9 @@
 namespace Controlink\LaravelWinmax4\app\Models;
 
 use Controlink\LaravelWinmax4\app\Models\Scopes\LicenseScope;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Winmax4Entity extends Model
 {
@@ -33,16 +33,23 @@ class Winmax4Entity extends Model
 
     protected static function booted()
     {
-        parent::booted();
+        parent::boot();
 
         if (config('winmax4.use_soft_deletes')) {
-            static::addGlobalScope('softDeletes', function (Builder $builder) {
-                $builder->withTrashed();
-            });
+            static::addTraitIfNotExists(SoftDeletes::class);
         }
 
-        if (config('winmax4.use_license') && !app()->runningInConsole()) {
+        if(config('winmax4.use_license') && !app()->runningInConsole()){
             static::addGlobalScope(new LicenseScope());
+        }
+    }
+
+    protected static function addTraitIfNotExists($trait)
+    {
+        $usedTraits = class_uses(static::class);
+
+        if (!in_array($trait, $usedTraits)) {
+            eval('namespace ' . __NAMESPACE__ . '; class ' . static::class . ' extends \\' . static::class . ' { use \\' . $trait . '; }');
         }
     }
 }
