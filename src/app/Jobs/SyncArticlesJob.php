9d7b42ecae6e075514e39b8c88adf04044993964
inesc <ines.pereira@controlink.pt>
2024-07-30 12:16:59 +0100

    Comment out unused synchronization logic
    Commented out large sections of synchronization logic related to articles and their associated data, such as taxes, prices, and stocks. This helps clarify the current focus of the SyncArticlesJob.php and temporarily removes unused code for potential future use.

--- src/app/Jobs/SyncArticlesJob.php
+++ src/app/Jobs/SyncArticlesJob.php
@@ -35,128 +35,128 @@ class SyncArticlesJob implements ShouldQueue
      */
     public function handle(): void
     {
-        if(config('winmax4.use_license')){
-            $article = Winmax4Article::updateOrCreate(
-                [
-                    'id_winmax4' => $this->article->ID,
-                    config('winmax4.license_column') => $this->license_id,
-                ],
-                [
-                    'code' => $this->article->Code,
-                    'designation' => $this->article->Designation ?? null,
-                    'short_description' => $this->article->ShortDescription ?? null,
-                    'is_active' => $this->article->IsActive,
-                    'family_code' => $this->article->FamilyCode,
-                    'sub_family_code' => $this->article->SubFamilyCode ?? null,
-                    'sub_sub_family_code' => $this->article->SubSubFamilyCode ?? null,
-                    'sub_sub_sub_family_code' => $this->article->SubSubSubFamilyCode ?? null,
-                    'stock_unit_code' => $this->article->StockUnitCode ?? null,
-                    'image_url' => $this->article->ImageUrl ?? null,
-                    'extras' => $this->article->Extras ?? null,
-                    'holds' => $this->article->Holds ?? null,
-                    'descriptives' => $this->article->Descriptives ?? null,
-                ]
-            );
-        }else{
-            $article = Winmax4Article::updateOrCreate(
-                [
-                    'id_winmax4' => $this->article->ID,
-                ],
-                [
-                    'code' => $this->article->Code,
-                    'designation' => $this->article->Designation ?? null,
-                    'short_description' => $this->article->ShortDescription ?? null,
-                    'is_active' => $this->article->IsActive,
-                    'family_code' => $this->article->FamilyCode,
-                    'sub_family_code' => $this->article->SubFamilyCode ?? null,
-                    'sub_sub_family_code' => $this->article->SubSubFamilyCode ?? null,
-                    'sub_sub_sub_family_code' => $this->article->SubSubSubFamilyCode ?? null,
-                    'stock_unit_code' => $this->article->StockUnitCode ?? null,
-                    'image_url' => $this->article->ImageUrl ?? null,
-                    'extras' => $this->article->Extras ?? null,
-                    'holds' => $this->article->Holds ?? null,
-                    'descriptives' => $this->article->Descriptives ?? null,
-                ]
-            );
-        }
-
-        if(isset($this->article->SaleTaxes)){
-            foreach ($this->article->SaleTaxes as $saleTax) {
-                Winmax4ArticleSaleTaxes::updateOrCreate(
-                    [
-                        'article_id' => $article->id,
-                        'tax_fee_code' => $saleTax->TaxFeeCode,
-                    ],
-                    [
-                        'percentage' => $saleTax->Percentage ?? 0,
-                        'fixedAmount' => $saleTax->FixedAmount ?? 0,
-                    ]
-                );
-            }
-        }
-
-        if(isset($this->article->PurchaseTaxes)){
-            foreach ($this->article->PurchaseTaxes as $purchaseTax) {
-                Winmax4ArticlePurchaseTaxes::updateOrCreate(
-                    [
-                        'article_id' => $article->id,
-                        'tax_fee_code' => $purchaseTax->TaxFeeCode,
-                    ],
-                    [
-                        'percentage' => $purchaseTax->Percentage ?? 0,
-                        'fixedAmount' => $purchaseTax->FixedAmount ?? 0,
-                    ]
-                );
-            }
-        }
-
-        if(isset($this->article->Prices)){
-            foreach ($this->article->Prices as $price) {
-                Winmax4ArticlePrices::updateOrCreate(
-                    [
-                        'article_id' => $article->id,
-                        'currency_code' => $price->CurrencyCode,
-                    ],
-                    [
-                        'sales_price1_without_taxes' => $price->SalesPrice1WithoutTaxes ?? 0,
-                        'sales_price1_with_taxes' => $price->SalesPrice1WithTaxes ?? 0,
-                        'sales_price2_without_taxes' => $price->SalesPrice2WithoutTaxes ?? 0,
-                        'sales_price2_with_taxes' => $price->SalesPrice2WithTaxes ?? 0,
-                        'sales_price3_without_taxes' => $price->SalesPrice3WithoutTaxes ?? 0,
-                        'sales_price3_with_taxes' => $price->SalesPrice3WithTaxes ?? 0,
-                        'sales_price4_without_taxes' => $price->SalesPrice4WithoutTaxes ?? 0,
-                        'sales_price4_with_taxes' => $price->SalesPrice4WithTaxes ?? 0,
-                        'sales_price5_without_taxes' => $price->SalesPrice5WithoutTaxes ?? 0,
-                        'sales_price5_with_taxes' => $price->SalesPrice5WithTaxes ?? 0,
-                        'sales_price6_without_taxes' => $price->SalesPrice6WithoutTaxes ?? 0,
-                        'sales_price6_with_taxes' => $price->SalesPrice6WithTaxes ?? 0,
-                        'sales_price7_without_taxes' => $price->SalesPrice7WithoutTaxes ?? 0,
-                        'sales_price7_with_taxes' => $price->SalesPrice7WithTaxes ?? 0,
-                        'sales_price8_without_taxes' => $price->SalesPrice8WithoutTaxes ?? 0,
-                        'sales_price8_with_taxes' => $price->SalesPrice8WithTaxes ?? 0,
-                        'sales_price9_without_taxes' => $price->SalesPrice9WithoutTaxes ?? 0,
-                        'sales_price9_with_taxes' => $price->SalesPrice9WithTaxes ?? 0,
-                        'sales_price_extra_without_taxes' => $price->SalesPriceExtraWithoutTaxes ?? 0,
-                        'sales_price_extra_with_taxes' => $price->SalesPriceExtraWithTaxes ?? 0,
-                        'sales_price_hold_without_taxes' => $price->SalesPriceHoldWithoutTaxes ?? 0,
-                        'sales_price_hold_with_taxes' => $price->SalesPriceHoldWithTaxes ?? 0,
-                    ]
-                );
-            }
-        }
-
-        if(isset($this->article->Stocks)){
-            foreach ($this->article->Stocks as $stock) {
-                Winmax4ArticleStocks::updateOrCreate(
-                    [
-                        'article_id' => $article->id,
-                        'warehouse_code' => $stock->WarehouseCode,
-                    ],
-                    [
-                        'current' => $stock->Current ?? 0,
-                    ]
-                );
-            }
-        }
+//        if(config('winmax4.use_license')){
+//            $article = Winmax4Article::updateOrCreate(
+//                [
+//                    'id_winmax4' => $this->article->ID,
+//                    config('winmax4.license_column') => $this->license_id,
+//                ],
+//                [
+//                    'code' => $this->article->Code,
+//                    'designation' => $this->article->Designation ?? null,
+//                    'short_description' => $this->article->ShortDescription ?? null,
+//                    'is_active' => $this->article->IsActive,
+//                    'family_code' => $this->article->FamilyCode,
+//                    'sub_family_code' => $this->article->SubFamilyCode ?? null,
+//                    'sub_sub_family_code' => $this->article->SubSubFamilyCode ?? null,
+//                    'sub_sub_sub_family_code' => $this->article->SubSubSubFamilyCode ?? null,
+//                    'stock_unit_code' => $this->article->StockUnitCode ?? null,
+//                    'image_url' => $this->article->ImageUrl ?? null,
+//                    'extras' => $this->article->Extras ?? null,
+//                    'holds' => $this->article->Holds ?? null,
+//                    'descriptives' => $this->article->Descriptives ?? null,
+//                ]
+//            );
+//        }else{
+//            $article = Winmax4Article::updateOrCreate(
+//                [
+//                    'id_winmax4' => $this->article->ID,
+//                ],
+//                [
+//                    'code' => $this->article->Code,
+//                    'designation' => $this->article->Designation ?? null,
+//                    'short_description' => $this->article->ShortDescription ?? null,
+//                    'is_active' => $this->article->IsActive,
+//                    'family_code' => $this->article->FamilyCode,
+//                    'sub_family_code' => $this->article->SubFamilyCode ?? null,
+//                    'sub_sub_family_code' => $this->article->SubSubFamilyCode ?? null,
+//                    'sub_sub_sub_family_code' => $this->article->SubSubSubFamilyCode ?? null,
+//                    'stock_unit_code' => $this->article->StockUnitCode ?? null,
+//                    'image_url' => $this->article->ImageUrl ?? null,
+//                    'extras' => $this->article->Extras ?? null,
+//                    'holds' => $this->article->Holds ?? null,
+//                    'descriptives' => $this->article->Descriptives ?? null,
+//                ]
+//            );
+//        }
+//
+//        if(isset($this->article->SaleTaxes)){
+//            foreach ($this->article->SaleTaxes as $saleTax) {
+//                Winmax4ArticleSaleTaxes::updateOrCreate(
+//                    [
+//                        'article_id' => $article->id,
+//                        'tax_fee_code' => $saleTax->TaxFeeCode,
+//                    ],
+//                    [
+//                        'percentage' => $saleTax->Percentage ?? 0,
+//                        'fixedAmount' => $saleTax->FixedAmount ?? 0,
+//                    ]
+//                );
+//            }
+//        }
+//
+//        if(isset($this->article->PurchaseTaxes)){
+//            foreach ($this->article->PurchaseTaxes as $purchaseTax) {
+//                Winmax4ArticlePurchaseTaxes::updateOrCreate(
+//                    [
+//                        'article_id' => $article->id,
+//                        'tax_fee_code' => $purchaseTax->TaxFeeCode,
+//                    ],
+//                    [
+//                        'percentage' => $purchaseTax->Percentage ?? 0,
+//                        'fixedAmount' => $purchaseTax->FixedAmount ?? 0,
+//                    ]
+//                );
+//            }
+//        }
+//
+//        if(isset($this->article->Prices)){
+//            foreach ($this->article->Prices as $price) {
+//                Winmax4ArticlePrices::updateOrCreate(
+//                    [
+//                        'article_id' => $article->id,
+//                        'currency_code' => $price->CurrencyCode,
+//                    ],
+//                    [
+//                        'sales_price1_without_taxes' => $price->SalesPrice1WithoutTaxes ?? 0,
+//                        'sales_price1_with_taxes' => $price->SalesPrice1WithTaxes ?? 0,
+//                        'sales_price2_without_taxes' => $price->SalesPrice2WithoutTaxes ?? 0,
+//                        'sales_price2_with_taxes' => $price->SalesPrice2WithTaxes ?? 0,
+//                        'sales_price3_without_taxes' => $price->SalesPrice3WithoutTaxes ?? 0,
+//                        'sales_price3_with_taxes' => $price->SalesPrice3WithTaxes ?? 0,
+//                        'sales_price4_without_taxes' => $price->SalesPrice4WithoutTaxes ?? 0,
+//                        'sales_price4_with_taxes' => $price->SalesPrice4WithTaxes ?? 0,
+//                        'sales_price5_without_taxes' => $price->SalesPrice5WithoutTaxes ?? 0,
+//                        'sales_price5_with_taxes' => $price->SalesPrice5WithTaxes ?? 0,
+//                        'sales_price6_without_taxes' => $price->SalesPrice6WithoutTaxes ?? 0,
+//                        'sales_price6_with_taxes' => $price->SalesPrice6WithTaxes ?? 0,
+//                        'sales_price7_without_taxes' => $price->SalesPrice7WithoutTaxes ?? 0,
+//                        'sales_price7_with_taxes' => $price->SalesPrice7WithTaxes ?? 0,
+//                        'sales_price8_without_taxes' => $price->SalesPrice8WithoutTaxes ?? 0,
+//                        'sales_price8_with_taxes' => $price->SalesPrice8WithTaxes ?? 0,
+//                        'sales_price9_without_taxes' => $price->SalesPrice9WithoutTaxes ?? 0,
+//                        'sales_price9_with_taxes' => $price->SalesPrice9WithTaxes ?? 0,
+//                        'sales_price_extra_without_taxes' => $price->SalesPriceExtraWithoutTaxes ?? 0,
+//                        'sales_price_extra_with_taxes' => $price->SalesPriceExtraWithTaxes ?? 0,
+//                        'sales_price_hold_without_taxes' => $price->SalesPriceHoldWithoutTaxes ?? 0,
+//                        'sales_price_hold_with_taxes' => $price->SalesPriceHoldWithTaxes ?? 0,
+//                    ]
+//                );
+//            }
+//        }
+//
+//        if(isset($this->article->Stocks)){
+//            foreach ($this->article->Stocks as $stock) {
+//                Winmax4ArticleStocks::updateOrCreate(
+//                    [
+//                        'article_id' => $article->id,
+//                        'warehouse_code' => $stock->WarehouseCode,
+//                    ],
+//                    [
+//                        'current' => $stock->Current ?? 0,
+//                    ]
+//                );
+//            }
+//        }
     }
 }
