4359f5802b8dfeecee1f4ae689b021be585c854c
inesc <ines.pereira@controlink.pt>
2024-07-30 12:49:17 +0100

    Add foreign key constraint in tax rates migration
    Updated the 'tax_id' column in the winmax4_taxes_rates table to include a foreign key constraint referencing the Winmax4Tax model. This change ensures referential integrity and enables cascading delete operations for related tax rate records.

--- src/database/migrations/2024_07_04_162800_create_winmax4_taxes_rates_table.php
+++ src/database/migrations/2024_07_04_162800_create_winmax4_taxes_rates_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('winmax4_taxes_rates', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\Controlink\LaravelWinmax4\app\Models\Winmax4Tax::class, 'tax_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\Controlink\LaravelWinmax4\app\Models\Winmax4Tax::class, 'tax_id')->cascadeOnDelete();
             $table->integer('fixedAmount');
             $table->integer('percentage');
 
