14ee8d0fbba991611ac96c5690b021b3905b8043
inesc <ines.pereira@controlink.pt>
2024-08-02 10:49:38 +0100

    Add language support for Winmax4 module
    Introduced English and Portuguese translation files for the Winmax4 module. This includes fields and success messages to improve localization efforts.

---
+++ src/resources/lang/en/winmax4.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'fields' => [
+        'url' => 'URL',
+        'company_code' => 'Company Code',
+        'username' => 'Username',
+        'password' => 'Password',
+        'n_terminal' => 'Nº Terminal',
+
+        'typeDocumentsForInvoice' => 'Type of Documents for Invoice',
+        'typeDocForInvoiceReceipt' => 'Type of Document for Invoice Receipt',
+        'creditNote' => 'Credit Note',
+        'receipt' => 'Receipt',
+    ],
+
+    'messages' => [
+        'success' => 'Winmax4 saved successfully!',
+    ],
+];
---
+++ src/resources/lang/pt/winmax4.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'fields' => [
+        'url' => 'URL',
+        'company_code' => 'Código da Empresa',
+        'username' => 'Nome de Utilizador',
+        'password' => 'Palavra-passe',
+        'n_terminal' => 'Nº Terminal',
+
+        'typeDocumentsForInvoice' => 'Tipo de Documentos para Fatura',
+        'typeDocForInvoiceReceipt' => 'Tipo de Documento para Recibo de Fatura',
+        'creditNote' => 'Nota de Crédito',
+        'receipt' => 'Recibo',
+    ],
+
+    'messages' => [
+        'success' => 'Winmax4 guardado com sucesso!',
+    ],
+];
