970d6c0b8ec834aaf928fbe6c9c53a0385006e59
inesc <ines.pereira@controlink.pt>
2024-09-20 14:50:08 +0100

    Fix format postDocuments

--- src/app/Services/Winmax4DocumentService.php
+++ src/app/Services/Winmax4DocumentService.php
@@ -2,6 +2,7 @@
 
 namespace Controlink\LaravelWinmax4\app\Services;
 
+use Controlink\LaravelWinmax4\app\Models\Winmax4Document;
 use Controlink\LaravelWinmax4\app\Models\Winmax4DocumentType;
 use Controlink\LaravelWinmax4\app\Models\Winmax4Entity;
 use Dflydev\DotAccessData\Data;
@@ -98,7 +99,7 @@ class Winmax4DocumentService extends Winmax4Service
      * @return object|array|null Returns the API response decoded from JSON, or null on failure
      * @throws GuzzleException If there is a problem with the HTTP request
      */
-    public function postDocuments(string $documentType, Winmax4Entity $entity, array $details): object|array|null
+    public function postDocuments(Winmax4DocumentType $documentType, Winmax4Entity $entity, array $details): object|array|null
     {
         $response = $this->client->post($this->url . '/Transactions/Documents', [
             'verify' => $this->settings['verify_ssl_guzzle'],
@@ -107,7 +108,7 @@ class Winmax4DocumentService extends Winmax4Service
                 'Content-Type' => 'application/json',
             ],
             'json' => [
-                'DocumentTypeCode' => $documentType,
+                'DocumentTypeCode' => $documentType->code,
                 'Entity' => [
                     'Code' => $entity->code,
                     'TaxPayerID' => $entity->tax_payer_id,
@@ -118,6 +119,46 @@ class Winmax4DocumentService extends Winmax4Service
         ]);
 
         $document = json_decode($response->getBody()->getContents());
+
+        $document = new Winmax4Document();
+        $document->document_type_id = $document->Data->DocumentTypeID;
+        $document->document_number = $document->Data->DocumentNumber;
+        $document->serie = $document->Data->Serie;
+        $document->number = $document->Data->Number;
+        $document->date = $document->Data->Date;
+        $document->external_identification = $document->Data->ExternalIdentification;
+        $document->currency_code = $document->Data->CurrencyCode;
+        $document->is_deleted = $document->Data->IsDeleted;
+        $document->user_login = $document->Data->UserLogin;
+        $document->terminal_code = $document->Data->TerminalCode;
+        $document->source_warehouse_code = $document->Data->SourceWarehouseCode;
+        $document->target_warehouse_code = $document->Data->TargetWarehouseCode;
+        $document->entity_id = $document->Data->EntityID;
+        $document->total_without_taxes = $document->Data->TotalWithoutTaxes;
+        $document->total_applied_taxes = $document->Data->TotalAppliedTaxes;
+        $document->total_with_taxes = $document->Data->TotalWithTaxes;
+        $document->total_liquidated = $document->Data->TotalLiquidated;
+        $document->load_address = $document->Data->LoadAddress;
+        $document->load_location = $document->Data->LoadLocation;
+        $document->load_zip_code = $document->Data->LoadZipCode;
+        $document->load_date_time = $document->Data->LoadDateTime;
+        $document->load_vehicle_license_plate = $document->Data->LoadVehicleLicensePlate;
+        $document->load_country_code = $document->Data->LoadCountryCode;
+        $document->unload_address = $document->Data->UnloadAddress;
+        $document->unload_location = $document->Data->UnloadLocation;
+        $document->unload_zip_code = $document->Data->UnloadZipCode;
+        $document->unload_date_time = $document->Data->UnloadDateTime;
+        $document->unload_country_code = $document->Data->UnloadCountryCode;
+        $document->hash_characters = $document->Data->HashCharacters;
+        $document->ta_doc_code_id = $document->Data->TADocCodeID;
+        $document->atcudd = $document->Data->ATCUD;
+        $document->table_number = $document->Data->TableNumber;
+        $document->table_split_number = $document->Data->TableSplitNumber;
+        $document->sales_person_code = $document->Data->SalesPersonCode;
+        $document->remarks = $document->Data->Remarks;
+        $document->save();
+
+
 
         dd($document->Data);
     }
