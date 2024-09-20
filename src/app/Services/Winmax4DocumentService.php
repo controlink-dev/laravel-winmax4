c605bf7ad332869591153db590f1b8b5b69f5755
inesc <ines.pereira@controlink.pt>
2024-09-20 16:16:03 +0100

    Fix

--- src/app/Services/Winmax4DocumentService.php
+++ src/app/Services/Winmax4DocumentService.php
@@ -153,7 +153,7 @@ class Winmax4DocumentService extends Winmax4Service
         $document->unload_country_code = $documentResponse->Data->UnloadCountryCode;
         $document->hash_characters = $documentResponse->Data->HashCharacters;
         $document->ta_doc_code_id = $documentResponse->Data->TADocCodeID ?? null;
-        $document->atcudd = $documentResponse->Data->ATCUD;
+        $document->atcudd = $documentResponse->Data->ATCUD ?? null;
         $document->table_number = $documentResponse->Data->TableNumber;
         $document->table_split_number = $documentResponse->Data->TableSplitNumber;
         $document->sales_person_code = $documentResponse->Data->SalesPersonCode;
