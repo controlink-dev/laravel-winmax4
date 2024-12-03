2cfbba0525ae4fb8fbd1bda2323b57933d53ced2
Inês Pereira <ines.pereira@controlink.pt>
2024-12-03 16:52:06 +0000

    Fix post articles and put articles

--- src/app/Services/Winmax4ArticleService.php
+++ src/app/Services/Winmax4ArticleService.php
@@ -139,6 +139,7 @@ class Winmax4ArticleService extends Winmax4Service
                 'headers' => [
                     'Authorization' => 'Bearer ' . $this->token->Data->AccessToken->Value,
                     'Content-Type' => 'application/json',
+                    'http_errors' => false,
                 ],
                 'json' => [
                     'Code' => $code,
@@ -170,7 +171,6 @@ class Winmax4ArticleService extends Winmax4Service
 
             $responseDecoded = json_decode($response->getBody()->getContents());
 
-            dd($responseDecoded);
             // If the response is not OK, we will update the article locally
             if($responseDecoded->Results[0]->Code !== self::WINMAX4_RESPONSE_OK){
                 $idWinmax4 = $builder->where('code', $code)->first()->id_winmax4;
