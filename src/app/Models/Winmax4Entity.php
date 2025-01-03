4fb976fcc92c48777382f53fa79231cfd3885189
Inês Pereira <ines.pereira@controlink.pt>
2025-01-03 17:22:53 +0000

    Add Notifiable in Winmax4Entity.php

--- src/app/Models/Winmax4Entity.php
+++ src/app/Models/Winmax4Entity.php
@@ -6,6 +6,7 @@ use Controlink\LaravelWinmax4\app\Models\Scopes\LicenseScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class Winmax4Entity extends Model
 {
