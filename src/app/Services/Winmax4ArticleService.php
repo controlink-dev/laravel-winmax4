bdaf847562fb1335a4b3d11dd9495b91e10756ae
Inês Pereira <ines.pereira@controlink.pt>
2025-01-15 12:49:22 +0000

    🧹 refactor(src/app/Services/Winmax4ArticleService.php): remove unused code for handling inactive article codes

--- src/app/Services/Winmax4ArticleService.php
+++ src/app/Services/Winmax4ArticleService.php
@@ -285,31 +285,6 @@ class Winmax4ArticleService extends Winmax4Service
                 $errorResponse = $e->getResponse();
                 $errorJson = json_decode($errorResponse->getBody()->getContents(), true);
 
-                // Check if the article code is already in use and is inactive, then update it
-                if($errorJson['Results'][0]['Code'] == 'ARTICLECODEINUSE'){
-                    $idWinmax4 = Winmax4Article::where('code', $code)->exists() ? Winmax4Article::where('code', $code)->first()->id_winmax4 : null;
-
-                    if($idWinmax4){
-                        if(Winmax4Article::where('code', $code)->first()->is_active == 0){
-                            $this->putArticles(
-                                $idWinmax4,
-                                $code,
-                                $familyCode,
-                                $vatCode,
-                                $vatRate,
-                                $priceWithoutVat,
-                                $priceWithVat,
-                                $subFamilyCode,
-                                $subSubFamilyCode,
-                                $stock,
-                                $is_active
-                            );
-                        }
-
-                        return Winmax4Article::where('code', $code)->first()->toArray();
-                    }
-                }
-
                 // Return the error JSON or handle it as needed
                 return [
                     'error' => true,
