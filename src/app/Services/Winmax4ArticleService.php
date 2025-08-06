bcfd23e858751bccc7b06ca0123d4bfa5de23ab1
inesc <inescunhachacatas@gmail.com>
2025-08-06 18:53:52 +0100

    🐛 fix(Winmax4ArticleService): remove debugging dump statement from response handling

--- src/app/Services/Winmax4ArticleService.php
+++ src/app/Services/Winmax4ArticleService.php
@@ -212,6 +212,8 @@ class Winmax4ArticleService extends Winmax4Service
             return $responseDecoded;
         }
 
+        dd($responseDecoded);
+
         $articleData = $responseDecoded->Data->Article;
         $subFamilyCode = property_exists($articleData, 'SubFamilyCode') ? $articleData->SubFamilyCode : null;
         $subSubFamilyCode = property_exists($articleData, 'SubSubFamilyCode') ? $articleData->SubSubFamilyCode : null;
