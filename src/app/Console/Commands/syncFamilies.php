5cdcd1bdd5421727616a44227fe85bfb7afbbf2c
Helder <helder.andrade@controlink.pt>
2026-04-15 09:38:41 +0100

    ♻️ refactor(syncFamilies): add tenant check for license-based syncing

--- src/app/Console/Commands/syncFamilies.php
+++ src/app/Console/Commands/syncFamilies.php
@@ -56,8 +56,10 @@ class syncFamilies extends Command
         }
 
         foreach ($winmax4Settings as $winmax4Setting) {
-            if(!$winmax4Setting->tenant){
-                continue;
+            if(config('winmax4.use_license')){
+                if(!$winmax4Setting->tenant){
+                    continue;
+                }
             }
 
             $this->info('Syncing families  for ' . $winmax4Setting->company_code . '...');
